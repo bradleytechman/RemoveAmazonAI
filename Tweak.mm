b68869f7b9690b0875fbf37c29746f3052e0e416
bradleytechman <bradleyrobloxgreen@gmail.com>
2025-06-22 13:51:06 -0400

    Initial Commit

--- Tweak.mm
+++ Tweak.mm
@@ -1,15 +1,13 @@
 #import <UIKit/UIKit.h>
 #import <rootless.h>
 
-// Hook ANPDockedTabBarViewController
+
 %hook ANPDockedTabBarViewController
 - (void)viewDidLayoutSubviews {
-    %orig; // Call original method
+    %orig; 
 
-    // Access the tab bar
     UITabBar *tabBar = self.tabBarController.tabBar ?: self.tabBar;
     if (tabBar) {
-        // Enumerate subviews to hide and disable interaction
         for (UIView *subview in tabBar.subviews) {
             if ([subview isKindOfClass:NSClassFromString(@"UITabBarSwappableImageView")] ||
                 [subview isKindOfClass:NSClassFromString(@"UITabBarButton")]) {
@@ -24,15 +22,15 @@
 // Fallback: Hook UITabBar globally for Amazon app
 %hook UITabBar
 - (void)layoutSubviews {
-    %orig; // Call original method
+    %orig; 
 
-    // Apply only in Amazon app
+    
     if ([[NSBundle mainBundle].bundleIdentifier isEqualToString:@"com.amazon.Amazon"]) {
         for (UIView *subview in self.subviews) {
             if ([subview isKindOfClass:NSClassFromString(@"UITabBarSwappableImageView")] ||
                 [subview isKindOfClass:NSClassFromString(@"UITabBarButton")]) {
-                subview.hidden = YES; // Hide the subview
-                subview.userInteractionEnabled = NO; // Disable user interaction
+                subview.hidden = YES; 
+                subview.userInteractionEnabled = NO; 
             }
         }
     }
